b72753d3e2bafa349ee0dc2d72d752a4071bbe4c
DarioJunior <dario.junior@mandarin.com.br>
2024-04-03 17:26:09 -0300

    feat: create post route to create an event

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -3,9 +3,15 @@ package rocketseat.com.passin.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import rocketseat.com.passin.dto.event.EventIdDTO;
+import rocketseat.com.passin.dto.event.EventRequestDTO;
+import rocketseat.com.passin.dto.event.EventResponseDTO;
 //import lombok.RequiredArgsConstructor;
 import rocketseat.com.passin.services.EventService;
 
@@ -20,8 +26,19 @@ public class EventController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> getEvent(@PathVariable String id) {
-        this.service.getEventDetail(id);
-        return ResponseEntity.ok("Sucesso");
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id) {
+        EventResponseDTO event = this.service.getEventDetail(id);
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(
+        @RequestBody EventRequestDTO body,
+        UriComponentsBuilder uriComponentsBuilder
+    ) {
+        EventIdDTO eventIdDTO = this.service.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.EventId()).toUri();
+        return ResponseEntity.created(uri).body(eventIdDTO);
     }
 }
--- src/main/java/rocketseat/com/passin/repositories/AttendeeRepository.java
+++ src/main/java/rocketseat/com/passin/repositories/AttendeeRepository.java
@@ -1,5 +1,3 @@
-package rocketseat.com.passin.repositories;
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
